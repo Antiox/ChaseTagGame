cbfcf15eebdd4b8ea452b917660ba2c34ece2683
Antiox <alexandre.nopre@gmail.com>
2021-03-18 16:11:38 +0100

    Addad main menu, pause menu and game over screen

---
+++ ChaseTagGame/Assets/GameScript.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameScript : MonoBehaviour
+{
+    public Text gamePausedLabel;
+    public Text gameOverLabel;
+    public Text pointCounterLabel;
+
+    private bool isPaused = false;
+    private bool isGameOver = false;
+    private float points = 0;
+
+    public void Start()
+    {
+        Time.timeScale = 1f;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            isPaused = !isPaused;
+            if (isPaused)
+                PauseGame();
+            else
+                UnpauseGame();
+        }
+
+        if(!isGameOver)
+            points += Time.deltaTime * 10;
+        pointCounterLabel.text = points.ToString("N0");
+    }
+
+    public void GameOver()
+    {
+        isGameOver = true;
+        gameOverLabel.enabled = true;
+        Time.timeScale = 0.01f;
+        Invoke("RestartGame", 3f * Time.timeScale);
+    }
+
+    public void PauseGame()
+    {
+        gamePausedLabel.enabled = true;
+        Time.timeScale = 0;
+    }
+
+    public void UnpauseGame()
+    {
+        gamePausedLabel.enabled = false;
+        Time.timeScale = 1f;
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
---
+++ ChaseTagGame/Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyScript : MonoBehaviour
+{
+    public Transform target;
+    public GameObject gameManager;
+
+    private NavMeshAgent navMeshAgent;
+    private Collider proximityTriggerCollider;
+
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        gameManager = GameObject.FindGameObjectWithTag("GameManager");
+        proximityTriggerCollider = GetComponentInChildren<Collider>();
+    }
+
+    void Update()
+    {
+        navMeshAgent.SetDestination(target.position);
+        Debug.Log(navMeshAgent.isOnNavMesh);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+            gameManager.GetComponent<GameScript>().GameOver();
+    }
+}
